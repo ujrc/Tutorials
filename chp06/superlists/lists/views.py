00ee9e97ff261ef5752b50a667bcd2fbef286ab9
ujrc <uwjearc@yahoo.com>
2016-02-08 21:49:19 -0600

    URLs for forms x2, moved code in views + tests, new URL

--- chp06/superlists/lists/views.py
+++ chp06/superlists/lists/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render,redirect
 from lists.models import ListItem
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        ListItem.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     ListItem.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'lists/home.html')
 
 def view_list(request):
     items=ListItem.objects.all()
     return render(request,'lists/list.html',{'items':items})
+
+
+def new_list(request):
+    ListItem.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
