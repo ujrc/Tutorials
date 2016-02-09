05dd1f713ed8f6614851ee32b6cc4008beafd39c
ujrc <uwjearc@yahoo.com>
2016-02-08 22:29:07 -0600

    Added list models and updated views and list/tests

--- chp06/superlists/lists/tests.py
+++ chp06/superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
-from lists.models import ListItem
+from lists.models import ListItem,List
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -56,16 +56,24 @@ class HomePageTest(TestCase):
 
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_=List()
+        list_.save()
+
         first_item = ListItem()
         first_item.text = 'The first (ever) list item'
+        first_item.list=list_
         first_item.save()
 
         second_item = ListItem()
         second_item.text = 'Item the second'
+        second_item.list=list_
         second_item.save()
+
+        saved_list=List.objects.first()
+        self.assertEqual(saved_list,list_)
 
         saved_items = ListItem.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -73,7 +81,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list,list_)
 
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
@@ -101,8 +111,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'lists/list.html')
 
     def test_displays_all_items(self):
-        ListItem.objects.create(text='itemey 1')
-        ListItem.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        ListItem.objects.create(text='itemey 1', list=list_)
+        ListItem.objects.create(text='itemey 2', list=list_)
         response = self.client.get('/lists/the-only-list-in-the-world/') #
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
