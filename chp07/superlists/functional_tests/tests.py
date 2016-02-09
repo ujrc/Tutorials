f327ecf35ad957963ef9c9efef0e842619f9095e
ujrc <uwjearc@yahoo.com>
2016-02-09 14:25:09 -0600

    Use bootstrap to improve layout

--- chp07/superlists/functional_tests/tests.py
+++ chp07/superlists/functional_tests/tests.py
@@ -1,8 +1,9 @@
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
