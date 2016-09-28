4f325d8ddc667da4b654196fee54886551db8e6b
ujrc <uwjearc@yahoo.com>
2016-09-28 12:34:43 -0500

    removed duplicate in weatherServive.js

--- djangulars/tweets/webtweets/static/webtweets/js/weather/weatherService.js
+++ djangulars/tweets/webtweets/static/webtweets/js/weather/weatherService.js
@@ -1,10 +1,6 @@
 app.service('cityService', function() {
     this.city = 'New York, NY'
 });
-app.service('weatherService', ['$resource', function($resource) {
-    this.GetWeather=function(city, days,apiKey) {
-       var weatherAPI=$resource("http://api.openweathermap.org/data/2.5/forecast/daily",{
-            callback:"JSON_CALLBACK"},{get:{method:"JSONP"}});
 
 
 app.service('weatherService', ["$resource", "$http", function($resource, $http) {
@@ -15,7 +11,7 @@ app.service('weatherService', ["$resource", "$http", function($resource, $http) 
         }, { get: { method: "JSONP" } });
         return weatherAPI.get({ q: city, cnt: days, appid: apiKey });
     };
-   
+
 }]);
 // app.factory('apiFactory',["$http",function($http) { 
 //     var obj = {content:null};
@@ -31,4 +27,3 @@ app.factory('searchFactory', ["$resource", function($resource) {
     var link = "http://services.odata.org/OData/OData.svc/Products?$format=json";
     return $resource(link, {});
 }]);
-
